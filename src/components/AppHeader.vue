abd86055ba4233abbbc42693bff732d935d43c99
Saraesp <saraesposito145@gmail.com>
2023-01-23 12:59:10 +0100

    impostazione struttura Header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -6,7 +6,17 @@ export default {
 
 <template lang="">
     <header>
-     
+        <!-- Creo una Navbar dove inserisco il Logo e l'Hamburger -->
+        <nav class="navbar bg-body-tertiary">
+            <!-- Logo -->
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                <img src="" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+                </a>
+            </div>
+            <!-- Burger -->
+
+        </nav>
     </header>
 </template>
 
