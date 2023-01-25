deceb6c44cc977f056d203520cadd5f8a7970ae6
Saraesp <saraesposito145@gmail.com>
2023-01-25 20:11:53 +0100

    fine impostazione content pagina

--- src/components/AppContent.vue
+++ src/components/AppContent.vue
@@ -13,32 +13,135 @@ export default {
         </div>
     </div>
     <div class="container">
-        <div class="center-section">
-            <p class="text-center">MENS GROOMING</p>
-            <h1 class="title text-center">Service</h1>
-            <div class="row">
-                <div class="col-4 text-center">
-                    <img src="../assets/image/avadabarbers-trimcut-icon-before.png" alt="">
-                    <h3 class="title">Trim 	&amp; Cut</h3>
-                    <p>Avada Barbers are experts in the lickety split trim and hair cut. Quick but careful and ridiculously good looking.</p>
+        <div class="center-section padding-50">
+            <div class="text-center">
+                <p class="">MENS GROOMING</p>
+                <h1 class="title ">Service</h1>
+                <div class="row">
+                    <div class="col-4 text-center">
+                        <img src="../assets/image/avadabarbers-trimcut-icon-before.png" alt="">
+                        <h3 class="title">Trim 	&amp; Cut</h3>
+                        <p>Avada Barbers are experts in the lickety split trim and hair cut. Quick but careful and ridiculously good looking.</p>
+                    </div>
+                    <div class="col-4 text-center">
+                        <img src="../assets/image/avadabarbers-washndry-icon.png" alt="">
+                        <h3 class="title">Trim 	&amp; Cut</h3>
+                        <p>Avada Barbers are experts in the lickety split trim and hair cut. Quick but careful and ridiculously good looking.</p>
+                    </div>
+                    <div class="col-4 text-center">
+                        <img src="../assets/image/avadabarbers-beardtrim-icon.png" alt="">
+                        <h3 class="title">Trim 	&amp; Cut</h3>
+                        <p>Avada Barbers are experts in the lickety split trim and hair cut. Quick but careful and ridiculously good looking.</p>
+                    </div>
                 </div>
-                <div class="col-4 text-center">
-                    <img src="../assets/image/avadabarbers-washndry-icon.png" alt="">
-                    <h3 class="title">Trim 	&amp; Cut</h3>
-                    <p>Avada Barbers are experts in the lickety split trim and hair cut. Quick but careful and ridiculously good looking.</p>
-                </div>
-                <div class="col-4 text-center">
-                    <img src="../assets/image/avadabarbers-beardtrim-icon.png" alt="">
-                    <h3 class="title">Trim 	&amp; Cut</h3>
-                    <p>Avada Barbers are experts in the lickety split trim and hair cut. Quick but careful and ridiculously good looking.</p>
-                </div>
+                <button class="btn-style">READ ABOUT OUR SERICES</button>
             </div>
         </div>
     </div>
+
+        <div class="second-section">
+            <div class="container">
+                <div class="text-center padding-50">
+                    <p>WE HAVE YOU COVERED</p>
+                    <h1 class="title">Avada Grooming Products</h1>
+                    <div class="row">
+                        <div class="col-3">
+                            <img src="../assets/image/brush_dark.png" alt="">
+                            <h3 class="title">Brush</h3>
+                            <p>&dollar;15.00</p>
+                        </div>
+                        <div class="col-3">
+                            <img src="../assets/image/scissors.png" alt="">
+                            <h3 class="title">Scissors</h3>
+                            <p>&dollar;85.00</p>
+                        </div>
+                        <div class="col-3">
+                            <img src="../assets/image/hot_oil_dark.png" alt="">
+                            <h3 class="title">Hot Oil</h3>
+                            <p>&dollar;15.00</p>
+                        </div>
+                        <div class="col-3">
+                            <img src="../assets/image/straight_razor_dark.png" alt="">
+                            <h3 class="title">Straight Razor</h3>
+                            <p>&dollar;30.00</p>
+                        </div>
+                    </div>
+                    <button class="btn-style">SHOP OUR PRODUCT RANGE</button>
+                </div>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="bg-black d-flex">
+                    <hr>
+                    <div class="image-bg col-4">
+                        <img src="../assets/image/shaving_butter.png" alt="">
+                    </div>   
+                    <div class="body-section col-4 padding-50">
+                        <p class="p-tit">THIS MONTH FEATURED PRODUCT</p>
+                        <h1 class="title">Shaving Butter</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque laoreet enim est, vel molestie dolor ultrices ut. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        <button class="btn-style">BUY NOW</button>
+                    </div>         
+            </div>
+        </div>
+        
+        <div class="container padding-50">
+            <div class="text-center">
+                <p class="color-beige">MENS GROOMING</p>
+                <h1 class="title mb-5">Service</h1>
+            </div>
+            <div class="row section-service">
+                <div class="col-4 text-center">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi mollis eleifend ante ut sagittis. Sed sodales, urna et imperdiet placerat, ex nisi aliquam orci, ac varius odio libero et arcu.</p>
+                    <img src="../assets/image/avadabarbers-trimcut-gallery6-400x400.jpg" alt="">
+                    <h3 class="title">John Doe</h3>
+                </div>
+                <div class="col-4 text-center">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi mollis eleifend ante ut sagittis. Sed sodales, urna et imperdiet placerat, ex nisi aliquam orci, ac varius odio libero et arcu.</p>
+                    <img src="../assets/image/avadabarbers-trimcut-gallery7.jpg" alt="">
+                    <h3 class="title">Pete Jones</h3>
+                </div>
+                <div class="col-4 text-center">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi mollis eleifend ante ut sagittis. Sed sodales, urna et imperdiet placerat, ex nisi aliquam orci, ac varius odio libero et arcu.</p>
+                    <img src="../assets/image/avadabarbers-trimcut-gallery3.jpg" alt="">
+                    <h3 class="title">Mark Wilson</h3>
+                </div>
+            </div>
+        </div>
+
+        <div class="container padding-50">
+            <div class="text-center">
+                <p class="color-beige">TALES FROM BARBER SHOP</p>
+                <h1 class="title mb-5">Recent Blog Posts</h1>
+                <div class="row section-blog">
+                    <div class="col-4 text-center">
+                        <div class="zoom">
+                            <img src="../assets/image/avadabarbers-second-blog-700x441.jpg" alt="">
+                        </div>
+                        <h3 class="title">Avada Barbers Now Open</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ut dapibus magna. Praesent semper,</p>
+                    </div>
+                    <div class="col-4 text-center">
+                        <img src="../assets/image/avadabarbers-choosing-blog-320x202.jpg" alt="">
+                        <h3 class="title">Choosing The Right Barber</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ut dapibus magna. Praesent semper,</p>
+                    </div>
+                    <div class="col-4 text-center">
+                        <img src="../assets/image/avadabarbers-ourservice-blog-320x202.jpg" alt="">
+                        <h3 class="title">Quick Service Guide</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ut dapibus magna. Praesent semper,</p>
+                    </div>
+                </div>
+                <button class="button btn-style">READ MORE ABOUT OUR BLOG</button>
+            </div>
+        </div>
+
 </template>
 
 <style lang="scss">
     @use '../style/partials/variables' as *;
+
     .bg-image{
         background-image: url('../assets/image/avadabarbers-about-introbackground.jpg');
         background-size: cover;
@@ -50,11 +153,6 @@ export default {
             font-weight: 600;
             margin-top: 40px;
         }
-
-        h1{
-            color: rgb(159, 156, 156);
-            font-size: 70px;
-        }
     } 
 
     p{
@@ -62,12 +160,6 @@ export default {
     }
 
     .center-section{
-        padding: 50px;
-
-        h1{
-            color: rgb(159, 156, 156);
-            font-size: 70px;
-        }
 
         .col-4{
             margin-top: 40px;
@@ -77,17 +169,134 @@ export default {
                 margin-bottom: 20px;
             }
 
-            h3{
-                color: $color-beige;
-            }
 
             p{
-                color: rgb(159, 156, 156);
                 line-height: 2;
                 width: 250px;
                 margin-left: 65px;
                 margin-top: 20px;
             }
         }
+
+        button{
+            font-weight: 600;
+            padding: 10px 20px;
+        }
     }
+
+    .second-section{
+        background-image: url('../assets/image/avadabarbers-reviewsbackground.jpg');
+        background-size: cover;
+
+        img{
+            width: 300px;
+        }
+
+        p{
+            color: $color-beige;
+        }
+
+        h1{
+            font-size: 60px;
+        }
+
+        button{
+            font-size: 20px;
+            font-weight: 600;
+            padding: 10px 30px;
+            margin-top: 30px;
+        }
+
+        padding-bottom: 150px ;
+    }
+
+    .bg-black{
+        border-top: 4px solid $color-beige;
+
+        h1{
+            font-size: 60px;
+        }
+
+        .p-tit{
+            color: $color-beige;
+        }
+
+        .body-section{
+            width: 700px;
+        }
+
+        button{
+            font-size: 20px;
+            font-weight: 600;
+            padding: 10px 30px;
+            margin-top: 30px;
+        }
+    }
+
+    .image-bg{
+        background-image: url('../assets/image/avadabarbers-cta-background.jpg');
+        background-size: cover;
+
+            img{
+                width: 350px;
+                margin: 30px;
+            }
+    }
+
+    .section-service{
+
+        img{
+            border-radius: 50%;
+            width: 110px;
+        }
+
+        h3{
+            font-size: 18px;
+            margin: 20px 0;
+        }
+
+        p{
+            width: 310px;
+            margin-left: 60px;
+            margin-bottom: 20px;
+            color: rgb(86, 85, 85);
+        }
+    }
+
+    .color-beige{
+        color: $color-beige;
+    }
+
+    .section-blog{
+
+        .zoom{
+            transition: transform .2s;
+
+        }
+        .zoom:hover{
+            transform: scale(1.2); 
+        }
+
+        img{
+            width: 380px;
+            margin-bottom: 30px;
+        }
+
+        h3{
+            font-size: 35px;
+        }
+
+        p{
+            width: 300px;
+            margin-left: 50px;
+        }
+
+    }
+     
+     .button{
+        font-weight: 500;
+        font-size: 18px;
+        padding: 10px 30px;
+        margin-top: 45px;
+     }
 </style>
